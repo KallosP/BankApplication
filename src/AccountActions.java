fc277daec9a38ce2e5931ad1393eb0d8b4014661
KallosP <pkallos19@gmail.com>
2022-08-12 20:03:08 -0700

    Cleaned Up Code
    Merged redundant code from CheckingAccountMenuOptions.java and SavingsAccountMenuOptions.java into AccountActions.java. Separated the code if option 5 is selected in the executeSavingsMenu method into a private method.

--- src/AccountActions.java
+++ src/AccountActions.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class AccountActions {
 	double currBalanceC; //checking amt
@@ -52,4 +53,102 @@ public class AccountActions {
 		currBalanceS -= withDrawAmtS;
 		return currBalanceS;
 	}
+	
+	public void manageDeposit(double depAmt, double balance, char acct, Scanner scnr) {
+		//FIXME: bug where viewing previous transaction allows for negative values
+
+		System.out.println("\nEnter the amount you would like to deposit:");
+		
+		if(scnr.hasNextDouble()) { //prevents crash from invalid user input
+			
+			depAmt = scnr.nextDouble(); 
+			
+			if(depAmt < 0) {
+				System.err.println("\nInvalid amount: No deposit made.");
+				System.out.println("\nCURRENT BALANCE: $" + balance + "\n");
+			}
+			else {
+				
+				//Sets deposit amt and balance of respective account
+				if(acct == 'C') {
+					depAmtC = depAmt;
+					currBalanceC = balance;
+					System.out.println("\nNEW TOTAL BALANCE: $" + makeAndReturnCheckingDeposit(depAmt) + "\n");
+				}
+				else if (acct == 'S') {
+					depAmtS = depAmt;
+					currBalanceS = balance;
+					System.out.println("\nNEW TOTAL BALANCE: $" + makeAndReturnSavingsDeposit(depAmt) + "\n");
+				}
+				
+			}
+		}
+		else {
+			System.err.println("\nInvalid amount: No deposit made.");
+			System.out.println("\nCURRENT BALANCE: $" + balance + "\n");
+		}
+		scnr.nextLine(); //used as a counter measure for the above caution (prevents printMenu() from executing twice)	
+		
+	}
+	
+	public void manageWithdrawal(double withdrawalAmt, double balance, char acct, Scanner scnr) {
+		
+		System.out.println("\nEnter the amount you would like to withdraw:");
+		
+		if(scnr.hasNextDouble()) {
+			withdrawalAmt = scnr.nextDouble();
+			if(withdrawalAmt < 0) {
+				System.err.println("\nInvalid amount: No withdrawal made.");
+				System.out.println("\nCURRENT BALANCE: $" + balance + "\n");
+			}
+			else {
+				
+				//Sets deposit amt and balance of respective account
+				if(acct == 'C') {
+					withDrawAmtC = withdrawalAmt;
+					currBalanceC = balance;
+					System.out.println("\nNEW TOTAL BALANCE: $" + makeAndReturnCheckingWithdrawal(withdrawalAmt) + "\n");
+				}
+				else if (acct == 'S') {
+					withDrawAmtS = withdrawalAmt;
+					currBalanceS = balance;
+					System.out.println("\nNEW TOTAL BALANCE: $" + makeAndReturnSavingsWithdrawal(withdrawalAmt) + "\n");
+				}
+				
+				
+			}
+		}
+		else {
+			System.err.println("\nInvalid amount: No withdrawal made.");
+		}
+		scnr.nextLine();
+		
+	}
+	
+	public void printPreviousTransaction(int transTracker, double depAmt, double withdrawalAmt, double balance) {
+		
+		if(transTracker == 0) {
+			if(depAmt == 0) {
+				System.err.println("\nNo transaction was made.");
+				System.out.println("\nCURRENT BALANCE: $" + balance + "\n");
+			}
+			else {
+				System.out.println("\nA deposit of $" + depAmt + " was made.");
+				System.out.println("\nCURRENT BALANCE: $" + balance + "\n");
+			}	
+		}
+		else if(transTracker == 1) {
+			
+			if(withdrawalAmt == 0) {
+				System.err.println("\nNo transaction was made.");
+				System.out.println("\nCURRENT BALANCE: $" + balance + "\n");
+			}
+			else {
+				System.out.println("\nA withdrawal of $" + withdrawalAmt + " was made.");
+				System.out.println("\nCURRENT BALANCE: $" + balance + "\n");
+			}
+		}
+		
+	}
+	
 }
--- src/SavingsAccountMenuOptions.java
+++ src/SavingsAccountMenuOptions.java
@@ -5,7 +5,7 @@ public class SavingsAccountMenuOptions {
 	LoadMenu loadMenu = new LoadMenu();
 	
 	public void executeSavingsMenu(String userIn, AccountActions AA, Scanner scnr) { //AA = acount actions
-		
+				
 		if(userIn.equals("1")) { //checks balance
 			
 			System.out.println("\nCURRENT BALANCE: $" + AA.getSavingsBalance() + "\n");
@@ -13,108 +13,58 @@ public class SavingsAccountMenuOptions {
 		}
 		else if(userIn.equals("2")) { //makes deposit
 			
-			System.out.println("\nEnter the amount you would like to deposit:");
-			if(scnr.hasNextDouble()) { //prevents crash from invalid user input
-				AA.depAmtS = scnr.nextDouble(); //CAUTION: returns extra new line which results in unwanted execution of printMenu()
-				if(AA.depAmtS < 0) {
-					System.err.println("\nInvalid amount: No deposit made.");
-					System.out.println("\nCURRENT BALANCE: $" + AA.currBalanceS + "\n");
-				}
-				else {
-					System.out.println("\nNEW TOTAL BALANCE: $" + AA.makeAndReturnSavingsDeposit(AA.depAmtS) + "\n");
-				}
-			}
-			else {
-				System.err.println("\nInvalid amount: No deposit made.");
-				System.out.println("\nCURRENT BALANCE: $" + AA.currBalanceS + "\n");
-			}
-			scnr.nextLine(); //used as a counter measure for the above caution (prevents printMenu() from executing twice)
+			AA.manageDeposit(AA.depAmtS, AA.currBalanceS, 'S', scnr);
 			
 		}
 		else if(userIn.equals("3")) { //makes withdrawal
 			
-			System.out.println("\nEnter the amount you would like to withdraw:");
-			//TODO: PREVENT CRASH IF NUMBER ISN'T ENTERED (maybe use do-while loop)
-			if(scnr.hasNextDouble()) {
-				AA.withDrawAmtS = scnr.nextDouble();
-				if(AA.withDrawAmtS < 0) {
-					System.err.println("\nInvalid amount: No withdrawal made.");
-					System.out.println("\nCURRENT BALANCE: $" + AA.currBalanceS + "\n");
-				}
-				else {
-					System.out.println("\nNEW TOTAL BALANCE: $" + AA.makeAndReturnSavingsWithdrawal(AA.withDrawAmtS) + "\n");
-				}
-			}
-			else {
-				System.err.println("\nInvalid amount: No withdrawal made.");
-			}
-			scnr.nextLine();
+			AA.manageWithdrawal(AA.withDrawAmtS, AA.currBalanceS, 'S', scnr);
 			
 		}
 		else if(userIn.equals("4")) { //prints previous transaction
 			
-		   if(AA.transactionTracker == 0) {
-				if(AA.depAmtS == 0) {
-					System.err.println("\nNo transaction was made.");
-					System.out.println("\nCURRENT BALANCE: $" + AA.currBalanceS + "\n");
-				}
-				else {
-					System.out.println("\nA deposit of $" + AA.depAmtS + " was made.");
-					System.out.println("\nCURRENT BALANCE: $" + AA.currBalanceS + "\n");
-				}	
-			}
-			else if(AA.transactionTracker == 1) {
-				
-				if(AA.withDrawAmtS == 0) {
-					System.err.println("\nNo transaction was made.");
-					System.out.println("\nCURRENT BALANCE: $" + AA.currBalanceS + "\n");
-				}
-				else {
-					System.out.println("\nA withdrawal of $" + AA.withDrawAmtS + " was made.");
-					System.out.println("\nCURRENT BALANCE: $" + AA.currBalanceS + "\n");
-				}
-			}
+			AA.printPreviousTransaction(AA.transactionTracker, AA.depAmtS, AA.withDrawAmtS, AA.currBalanceS);
 			
 		}
 		else if(userIn.equals("5")) {
+						
+			startInterestCalculator(scnr);
+			scnr.nextLine();
 			
-			int initDeposit;
-			double numMonthsInYears;
-			double interestRate;
+		}
+	}
+	
+	private void startInterestCalculator(Scanner scnr) {
+		
+		int initDeposit;
+		double numMonthsInYears;
+		double interestRate;
+		
+		
+		loadMenu.printICTitle();
+		
+		System.out.println("Starting Balance:");
+		if(scnr.hasNextInt()) {
 			
+			initDeposit = scnr.nextInt();
 			
-			loadMenu.printICTitle();
-			
-			System.out.println("Starting Balance:");
-			if(scnr.hasNextInt()) {
+			System.out.println("\nNumber of Months:");
+			if(scnr.hasNextDouble()) {
 				
-				initDeposit = scnr.nextInt();
+				numMonthsInYears = (double) scnr.nextInt() / 12;
 				
-				System.out.println("\nNumber of Months:");
+				System.out.println("\nAnnual Interest Rate:");
 				if(scnr.hasNextDouble()) {
 					
-					numMonthsInYears = (double) scnr.nextInt() / 12;
+					interestRate = scnr.nextDouble() / 100;
 					
-					System.out.println("\nAnnual Interest Rate:");
-					if(scnr.hasNextDouble()) {
-						
-						interestRate = scnr.nextDouble() / 100;
-						
-						//If all input is valid, calculate and print the total savings
-						calculateAndPrintTotalSavings(initDeposit, numMonthsInYears, interestRate);
-						
-					}
-					else {
-						System.err.println("\nInvalid entry.");
-						//Prevents Savings menu from printing more than once
-						scnr.nextLine();
-						scnr.nextLine(); 
-					}
+					//If all input is valid, calculate and print the total savings
+					calculateAndPrintTotalSavings(initDeposit, numMonthsInYears, interestRate);
 					
 				}
 				else {
 					System.err.println("\nInvalid entry.");
-					scnr.nextLine();
+					//Prevents Savings menu from printing more than once
 					scnr.nextLine();
 				}
 				
@@ -125,11 +75,16 @@ public class SavingsAccountMenuOptions {
 			}
 			
 		}
+		else {
+			System.err.println("\nInvalid entry.");
+			scnr.nextLine();
+		}
+		
 	}
 	
 	private void calculateAndPrintTotalSavings(int deposit, double months, double apy) {
 		
-		System.out.println("\nFuture Savings: $" + (double) Math.round( ( (double) deposit * (1 + ((double) apy * months)) ) * 100) / 100);
+		System.out.println("\nFuture Savings: $" + (double) Math.round( ( (double) deposit * (1 + ((double) apy * months)) ) * 100) / 100 + "\n");
 		
 	}
 	
