8f47d72af18ca4f3389db3a4f93a0bcfe35533b3
KallosP <97070073+KallosP@users.noreply.github.com>
2022-11-06 23:36:23 -0800

    Update LoadMenu.java

--- src/LoadMenu.java
+++ src/LoadMenu.java
@@ -1,5 +1,4 @@
 public class LoadMenu {
-	
 	public boolean checkingPromptOneExecuted = false;
 	public boolean savingsPromptOneExecuted = false;
 	
@@ -16,7 +15,7 @@ public class LoadMenu {
 				+ "\n2. Make a deposit"
 				+ "\n3. Make a withdrawal"
 				+ "\n4. View previous transaction"
-				+ "\n5. Simple Interest Calculator"
+				+ "\n5. Interest Calculator"
 				+ "\n6. Exit Savings Account");
 	}
 	
@@ -28,22 +27,21 @@ public class LoadMenu {
 	public void printWelcome() {
 		System.out.println("____________________________________________");
 		System.out.println();
-		System.out.println("        WELCOME TO YOUR BANK ACCOUNT      ");
+		System.out.println("  ~~~~~~WELCOME TO YOUR BANK ACCOUNT~~~~~~");
 		System.out.println("____________________________________________");
 		System.out.println();
-		System.out.println("    |To perform an action, please enter|"
-				   	   + "\n    |its corresponding number.         |\n");
+		System.out.println(" |To perform an action, please enter|"
+				   	   + "\n |its corresponding number.         |\n");
 		System.out.println("============================================");
 	}
 	
 	public void printFirstPrompt() {
-		System.out.println("Choose the account you wish to enter."); 
+		System.out.println("Choose the account you wish to enter."); //TODO: maybe format better
 		System.out.println("\n1. Checking Account\n2. Savings Account\n3. Log Out");
 	}
 	
 	public void printCheckingPrompt() {
-		//Checks if initial prompt has been executed
-		if(!checkingPromptOneExecuted) { 
+		if(!checkingPromptOneExecuted) { //Checks if initial prompt has been executed
 			System.out.println("CHECKING ACCOUNT:\n");
 			System.out.println("What would you like to do today?\n");
 		}
@@ -73,5 +71,4 @@ public class LoadMenu {
 	public void newMenuSignal() {
 		System.out.println("============================================");
 	}
-	
 }
